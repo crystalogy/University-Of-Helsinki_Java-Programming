4d884cd243a574aa7bc839b3471c24a40d375ef2
crystalogy <crystalramos.cmr@gmail.com>
2024-05-23 14:25:06 -0400

    Part 5 exercise: Comparing apartments

--- part05-Part05_11.ComparingApartments/src/main/java/Apartment.java
+++ part05-Part05_11.ComparingApartments/src/main/java/Apartment.java
@@ -3,12 +3,48 @@ public class Apartment {
 
     private int rooms;
     private int squares;
-    private int princePerSquare;
+    private int pricePerSquare;
 
     public Apartment(int rooms, int squares, int pricePerSquare) {
         this.rooms = rooms;
         this.squares = squares;
-        this.princePerSquare = pricePerSquare;
+        this.pricePerSquare = pricePerSquare;
+    }
+
+    public int pricePerSquare(int price){
+    return price * this.squares;
+    }
+    
+
+    public boolean largerThan(Apartment compared) {
+        if (this.squares > compared.squares) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public int priceDifference(Apartment compared) {
+        int price, comparedPrice;
+        price = this.pricePerSquare * this.squares;
+        comparedPrice = compared.pricePerSquare * compared.squares;
+        if (price > comparedPrice){
+            return price - comparedPrice;
+        } else {
+            return comparedPrice - price;
+        }
+    }
+    
+    
+    public boolean moreExpensiveThan(Apartment compared){
+        int currentApt = this.squares * compared.pricePerSquare;
+        int comparedApt = compared.squares * compared.pricePerSquare;
+        
+        if (currentApt > comparedApt){
+            return true;
+        } else {
+            return false;
+        }
     }
 
 }
