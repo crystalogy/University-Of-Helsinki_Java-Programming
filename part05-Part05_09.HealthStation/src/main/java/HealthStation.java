5a33c76accbecf54d2ebb77287df30bec5cb7433
crystalogy <crystalramos.cmr@gmail.com>
2024-05-23 12:33:41 -0400

    Part 5: exercises: HealthStation and PaymentCard

--- part05-Part05_09.HealthStation/src/main/java/HealthStation.java
+++ part05-Part05_09.HealthStation/src/main/java/HealthStation.java
@@ -1,10 +1,21 @@
 
 public class HealthStation {
+    private int weighings;
 
 
     public int weigh(Person person) {
-        // return the weight of the person passed as the parameter
-        return -1;
+        weighings++;
+        return person.getWeight();
+    }
+    
+    public void feed(Person person){
+        int weight = person.getWeight();
+        weight++;
+        person.setWeight(weight);
+    }
+    
+    public int weighings(){
+        return weighings;
     }
 
 }
