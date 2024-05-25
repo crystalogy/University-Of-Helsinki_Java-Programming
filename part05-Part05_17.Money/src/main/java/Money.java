9dc0e59716a8b305ba887c3f7d36e1c4e44ddafe
crystalogy <crystalramos.cmr@gmail.com>
2024-05-25 13:11:15 -0400

    part 5 - #17

--- part05-Part05_17.Money/src/main/java/Money.java
+++ part05-Part05_17.Money/src/main/java/Money.java
@@ -6,19 +6,49 @@ public class Money {
 
     public Money(int euros, int cents) {
 
-        if (cents > 99) {
-            euros = euros + cents / 100;
+        if (cents >= 100) {
+            euros += cents / 100;
             cents = cents % 100;
         }
 
         this.euros = euros;
         this.cents = cents;
     }
+
+    public Money plus(Money addition) {
+        int newEuros = this.euros + addition.euros;
+        int newCents = this.cents + addition.cents;
+
+        if (newCents >= 100) {
+            newEuros += newCents / 100;
+            newCents = newCents % 100;
+        }
+        return new Money(newEuros, newCents);
+    }
     
-    public Money plus(Money addition){
-        Money newMoney = new Money(this.euros, this.cents);
-        
-        return newMoney;
+    public boolean lessThan(Money compared){
+        if(this.euros < compared.euros){
+            return true;
+        } else if (this.euros == compared.euros){
+            return this.cents < compared.cents;
+        } else {
+        return false;
+        }
+    }
+    
+    public Money minus(Money decreaser){
+       int totalCenter = (this.euros * 100) + this.cents;
+       int decreaserTotalCenter = (decreaser.euros * 100) + decreaser.cents;
+       int difference = totalCenter - decreaserTotalCenter;
+       
+       if (difference <= 0){
+           return new Money(0,0);
+       }
+       
+       int newEuros = difference / 100;
+       int newCents = difference % 100;
+       
+       return new Money(newEuros, newCents);
     }
 
     public int euros() {
