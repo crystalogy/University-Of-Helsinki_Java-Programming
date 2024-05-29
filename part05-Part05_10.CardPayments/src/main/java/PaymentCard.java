01e53b44bc8bef9bf3282f9071196950cf785c47
Crystal Ramos <152758155+crystalogy@users.noreply.github.com>
2024-05-29 00:16:43 -0400

    Update PaymentCard.java

--- part05-Part05_10.CardPayments/src/main/java/PaymentCard.java
+++ part05-Part05_10.CardPayments/src/main/java/PaymentCard.java
@@ -16,9 +16,6 @@ public class PaymentCard {
     }
 
     public boolean takeMoney(double amount) {
-        // implement the method so that it only takes money from the card if
-        // the balance is at least the amount parameter.
-        // returns true if successful and false otherwise
         if (amount <= balance){
             balance -= amount;
             return true;
