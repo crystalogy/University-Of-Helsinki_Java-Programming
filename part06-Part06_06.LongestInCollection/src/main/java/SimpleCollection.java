1968c84658ff439733050d06063f4ea536b355cf
crystalogy <crystalramos.cmr@gmail.com>
2024-05-29 23:18:56 -0400

    exercise: Longest in collection

--- part06-Part06_06.LongestInCollection/src/main/java/SimpleCollection.java
+++ part06-Part06_06.LongestInCollection/src/main/java/SimpleCollection.java
@@ -18,5 +18,21 @@ public class SimpleCollection {
     public ArrayList<String> getElements() {
         return this.elements;
     }
+    
+    public String longest(){
+        
+        if (elements.isEmpty()){
+            return null;
+        }
+        
+        String longest = elements.get(0);
+        
+        for (String element : elements){
+            if (element.length() > longest.length()){
+                longest = element;
+            }
+        }
+        return longest;
+    }
 
 }
